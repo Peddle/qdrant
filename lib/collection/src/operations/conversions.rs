66b79678f30a00ac4466a244be5658a296b2a7c9
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-08-01 15:51:10 +0200

    include number of indexed points into the info api

--- lib/collection/src/operations/conversions.rs
+++ lib/collection/src/operations/conversions.rs
@@ -52,11 +52,13 @@ impl From<api::grpc::qdrant::OptimizersConfigDiff> for OptimizersConfigDiff {
 }
 
 impl From<CollectionInfo> for api::grpc::qdrant::CollectionInfo {
+    #[allow(deprecated)]
     fn from(value: CollectionInfo) -> Self {
         let CollectionInfo {
             status,
             optimizer_status,
             vectors_count,
+            indexed_vectors_count,
             points_count,
             segments_count,
             disk_data_size,
@@ -82,6 +84,7 @@ impl From<CollectionInfo> for api::grpc::qdrant::CollectionInfo {
                 }
             }),
             vectors_count: vectors_count as u64,
+            indexed_vectors_count: Some(indexed_vectors_count as u64),
             points_count: points_count as u64,
             segments_count: segments_count as u64,
             disk_data_size: disk_data_size as u64,
@@ -237,6 +240,7 @@ impl TryFrom<api::grpc::qdrant::CollectionConfig> for CollectionConfig {
 impl TryFrom<api::grpc::qdrant::GetCollectionInfoResponse> for CollectionInfo {
     type Error = Status;
 
+    #[allow(deprecated)]
     fn try_from(
         collection_info_response: api::grpc::qdrant::GetCollectionInfoResponse,
     ) -> Result<Self, Self::Error> {
@@ -255,6 +259,7 @@ impl TryFrom<api::grpc::qdrant::GetCollectionInfoResponse> for CollectionInfo {
                     }
                 },
                 vectors_count: collection_info_response.vectors_count as usize,
+                indexed_vectors_count: collection_info_response.indexed_vectors_count.unwrap_or_default() as usize,
                 points_count: collection_info_response.points_count as usize,
                 segments_count: collection_info_response.segments_count as usize,
                 disk_data_size: collection_info_response.disk_data_size as usize,
